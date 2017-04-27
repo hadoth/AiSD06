c913c4a3bddaf74222d6ebc50a9ecaec65bad17b
Karol <pokomeda.karol@gmail.com>
2017-04-27 17:20:59 +0200

    Implement QuickSort

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,27 @@
+package sort;
+
+import utils.comparator.Comparator;
+
+import java.util.List;
+
+/**
+ * Created by Karol Pokomeda on 2017-04-27.
+ */
+public class QuickSort<T> implements ListSorter<T> {
+
+
+    @Override
+    public List<T> sort(List<T> list) {
+        return null;
+    }
+
+    @Override
+    public void setComparator(Comparator<T> comparator) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
