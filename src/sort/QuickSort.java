7e3cba9f926ffe7ddf82184532e433863a31b4da
Karol <pokomeda.karol@gmail.com>
2017-04-27 23:25:02 +0200

    Fix QuickSort; move assert functionality to separate method

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -34,10 +34,7 @@ public class QuickSort<T> implements ListSorter<T> {
     private void quickSort(List<T> list, int startIndex, int endIndex){
         if (startIndex < endIndex){
             int partitionIndex = this.partition(list, startIndex, endIndex);
-            for (int i = 0; i < partitionIndex; i++) if (comparator.compare(list.get(i), list.get(partitionIndex))>0)
-                throw new AssertionError(list.get(i) + " greater than or equal to " + list.get(partitionIndex));
-            for (int i = partitionIndex + 1; i < list.size(); i++) if (comparator.compare(list.get(partitionIndex), list.get(i))>0)
-                throw new AssertionError(list.get(partitionIndex) + " is less than " + list.get(i) + "(" + comparator.compare(list.get(partitionIndex), list.get(i)) + ")");
+            this.assertPartition(list, partitionIndex);
             this.quickSort(list, startIndex, partitionIndex-1);
             this.quickSort(list, partitionIndex+1, endIndex);
         }
@@ -57,16 +54,36 @@ public class QuickSort<T> implements ListSorter<T> {
                 continue;
             }
             this.swap(list, start, end);
-            System.out.println(Arrays.toString(list.toArray()) + "["+ startIndex + "; " + endIndex +"]");
             start++;
         }
-        if (this.comparator.compare(list.get(start), elementToSort) > 0) this.swap(list, endIndex, start);
-        return start;
+        if (this.comparator.compare(list.get(start), elementToSort) >= 0) {
+            this.swap(list, endIndex, start);
+            return start;
+        }
+        return ++start;
     }
 
     private static <T> void swap(List<T> list, int left, int right){
         T leftElem = list.get(left);
         list.set(left, list.get(right));
         list.set(right, leftElem);
+    }
+
+    private void assertPartition(List<T> list, int partitionIndex){
+        for (int i = 0; i < partitionIndex; i++)
+            if (this.comparator.compare(list.get(i), list.get(partitionIndex))>0) {
+                throw new AssertionError(
+                        list.get(i) + " greater than or equal to " + list.get(partitionIndex) +
+                                '\n' + Arrays.toString(list.toArray()) +
+                                '\n' + partitionIndex
+                );
+            }
+        for (int i = partitionIndex + 1; i < list.size(); i++)
+            if (this.comparator.compare(list.get(partitionIndex), list.get(i))>0)
+                throw new AssertionError(
+                        list.get(partitionIndex) + " is less than " + list.get(i) +
+                                '\n' + Arrays.toString(list.toArray()) +
+                                '\n' + partitionIndex
+                );
     }
 }
