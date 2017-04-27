68f3b3ecd1a969e3421b3963f47fd3da6a76646f
Karol <pokomeda.karol@gmail.com>
2017-04-27 21:07:42 +0200

    Implement sort method for QuickSort

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -16,7 +16,8 @@ public class QuickSort<T> implements ListSorter<T> {
 
     @Override
     public List<T> sort(List<T> list) {
-        return null;
+        this.quickSort(list, 0, list.size()-1);
+        return list;
     }
 
     @Override
@@ -27,5 +28,38 @@ public class QuickSort<T> implements ListSorter<T> {
     @Override
     public String getName() {
         return "Quick Sort";
+    }
+
+    private void quickSort(List<T> list, int startIndex, int endIndex){
+        if (startIndex < endIndex){
+            int partitionIndex = this.partition(list, startIndex, endIndex);
+            this.quickSort(list, startIndex, partitionIndex-1);
+            this.quickSort(list, partitionIndex+1, endIndex);
+        }
+    }
+
+    private int partition(List<T> list, int startIndex, int endIndex){
+        T elementToSort = list.get(endIndex - 1);
+        int start = startIndex;
+        int end = endIndex-1;
+        while (start < end){
+            if (comparator.compare(list.get(start), elementToSort) < 0) {
+                start++;
+                continue;
+            }
+            if (comparator.compare(elementToSort, list.get(end)) >= 0){
+                end++;
+                continue;
+            }
+            this.swap(list, start, end);
+        }
+        this.swap(list, endIndex, start);
+        return start;
+    }
+
+    private static <T> void swap(List<T> list, int left, int right){
+        T leftElem = list.get(left);
+        list.set(left, list.get(right));
+        list.set(right, leftElem);
     }
 }
