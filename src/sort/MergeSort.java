3932576c7c26dcf4cd90c566b8629db2561fbb22
Karol <pokomeda.karol@gmail.com>
2017-04-28 06:59:36 +0200

    Implement MergeSort

---
+++ src/sort/MergeSort.java
@@ -0,0 +1,25 @@
+package sort;
+
+import utils.comparator.Comparator;
+
+import java.util.List;
+
+/**
+ * Created by Karol Pokomeda on 2017-04-28.
+ */
+public class MergeSort<T> implements ListSorter<T> {
+    @Override
+    public List<T> sort(List<T> list) {
+        return null;
+    }
+
+    @Override
+    public void setComparator(Comparator<T> comparator) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
