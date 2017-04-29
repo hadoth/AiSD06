8461dfa85614a538c5d449c84b6365b98ae27191
hadoth <valout13>
2017-04-29 21:06:24 +0200

    Implement QueueSort

---
+++ src/sort/QueueSort.java
@@ -0,0 +1,25 @@
+package sort;
+
+import utils.comparator.Comparator;
+
+import java.util.List;
+
+/**
+ * Created by Karol on 2017-04-29.
+ */
+public class QueueSort<T> implements ListSorter<T> {
+    @Override
+    public List<T> sort(List<T> list) {
+        return null;
+    }
+
+    @Override
+    public void setComparator(Comparator<T> comparator) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
