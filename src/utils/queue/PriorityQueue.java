c72a65b52b0344facdf260f713a6faf6e74d1c68
Karol <pokomeda.karol@gmail.com>
2017-04-29 06:42:16 +0200

    Redefine PriorityQueue interface

--- src/utils/queue/PriorityQueue.java
+++ src/utils/queue/PriorityQueue.java
@@ -5,6 +5,10 @@ import java.util.Queue;
 /**
  * Created by Karol Pokomeda on 2017-04-28.
  */
-public interface PriorityQueue<T> extends Queue<T>{
-
+public interface PriorityQueue<T>{
+    int size();
+    boolean isEmpty();
+    boolean add(T t);
+    T remove();
+    T peek();
 }
