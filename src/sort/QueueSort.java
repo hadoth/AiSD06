3a1b61c4b058f57959efd6579f392b6663801ebb
hadoth <valout13>
2017-04-29 21:13:04 +0200

    Implement QueueSort functionality

--- src/sort/QueueSort.java
+++ src/sort/QueueSort.java
@@ -3,23 +3,38 @@ package sort;
 import utils.comparator.Comparator;
 
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Created by Karol on 2017-04-29.
  */
 public class QueueSort<T> implements ListSorter<T> {
+    private Comparator<T> comparator;
+    private PriorityQueue<T> priorityQueue;
+
+    public QueueSort(Comparator<T> comparator, PriorityQueue<T> priorityQueue){
+        this.comparator = comparator;
+        this.priorityQueue = priorityQueue;
+    }
+
     @Override
     public List<T> sort(List<T> list) {
-        return null;
+        while (!list.isEmpty()) this.priorityQueue.add(list.remove(0));
+        while (!this.priorityQueue.isEmpty()) list.add(0, this.priorityQueue.remove());
+        return list;
     }
 
     @Override
     public void setComparator(Comparator<T> comparator) {
+        this.comparator = comparator;
+    }
 
+    public void setPriorityQueue(PriorityQueue<T> priorityQueue) {
+        this.priorityQueue = priorityQueue;
     }
 
     @Override
     public String getName() {
-        return null;
+        return "Queue Sort (" + this.priorityQueue.getClass().getSimpleName() + ")";
     }
 }
